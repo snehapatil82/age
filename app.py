9b85164e86ed74d7352fc7bff48849ba204c4ded
snehapatil82 <snehaspatil82@gmail.com>
2026-08-03 10:56:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Age validation")
+age=st.number_input("Enter your age:")
+if st.button("submit"):
+  if st.age>=18:
+    st.success("you are eligible to vote")
+  else:
+    st.write("you are not eligible to vote")
+  
