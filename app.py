700f8deedb7d0e96d88a536d1ac13215d2118bba
snehapatil82 <snehaspatil82@gmail.com>
2026-08-03 11:06:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 st.title("Age validation")
 age=st.number_input("Enter your age:")
 if st.button("submit"):
-  if np.age>=18:
+  if age>=18:
     st.success("you are eligible to vote")
   else:
     st.write("you are not eligible to vote")
